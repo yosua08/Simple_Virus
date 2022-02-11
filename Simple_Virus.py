7b1216ad81409b06c6b0a28b979a9e53f3c9d157
Yosua Putra Pratama <72484891+yosua08@users.noreply.github.com>
2022-02-11 23:29:21 +0700

    Master File
    Ini adalah file utama.

---
+++ Simple_Virus.py
@@ -0,0 +1,23 @@
+import pyautogui as pg
+import time
+
+"""
+Hi, saya Yozu.
+Source code ini sedikit berbahaya, ini dapat membuat perangkat anda error.
+Jadi berhati-hatilah ketika menggunakannya.
+
+--- SANGAT TIDAK DISARANKAN UNTUK MENJALANKANNYA ---
+"""
+
+time.sleep(5)
+
+for i in range(5):
+    pg.write("-------------------------------------------------------------------------------\n")
+    pg.write("||  =   = ==== ==== =  =    ||    === ====      ==== ==== ===== = ==== ====  ||\n")
+    pg.write("||   = =  =  =  =   =  =    ||     =  =  =      =  = =      =   = =  = =  =  ||\n")
+    pg.write("||    =   =  =   =  =  =    ||     =  =  =      ==== =      =   = =  = =  =  ||\n")
+    pg.write("||    =   ==== ==== ====    ||    === =  =      =  = ====   =   = ==== =  =  ||\n")
+    pg.write("-------------------------------------------------------------------------------\n")
+
+while True:
+   pg.write("Hacker Yozu in action..!\n")
